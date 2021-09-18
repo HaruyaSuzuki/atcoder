7af82559abe49987da22877b931c1eebca698ab9
HaruyaSuzuki <system.noteit@gmail.com>
2021-09-18 22:46:35 +0900

    2021.9.18

--- Contests/ABC219/B.cpp
+++ Contests/ABC219/B.cpp
@@ -5,25 +5,23 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+using 		namespace std;
 
-int		main(void)
+int				main(void)
 {
-	int	i;
-	string s1, s2, s3, t, ans;
+	int			i;
+	string 	s1, s2, s3, t, ans;
 
 	cin >> s1;
 	cin >> s2;
 	cin >> s3;
 	cin >> t;
-
 	for (i=0;i<t.length();i++)
 	{
 		if (t[i] == '1') ans += s1;
 		else if(t[i] == '2') ans += s2;
 		else if(t[i] == '3') ans += s3;
 	}
-
 	cout << ans;
 	return (0);
 }
